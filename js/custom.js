853e30927334e05e62a157ea089e8fdba6e45ff1
unknown <gdesouza0688@gmail.com>
2021-02-21 22:58:00 +0000

    first commit

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+$(function () {
+
+  // Function for the menu
+  $('.navbar-collapse a').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+  // AOS ANIMATION
+  AOS.init({
+    disable: 'mobile',
+    duration: 800,
+    anchorPlacement: 'center-bottom'
+  });
+
+
+  // SMOOTHSCROLL NAVBAR
+  $(function () {
+    $('.navbar a, .hero-text a').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
+});
+
+// ANIMATED TOPBAR
+
+const buttons = document.querySelectorAll('.project');
+const overlay = document.querySelector('.overlay');
+const overlayImage = document.querySelector('.overlay__inner img');
+
+function open(e) {
+  overlay.classList.add('open');
+  const src= e.currentTarget.querySelector('img').src;
+  overlayImage.src = src;
+}
+
+function close() {
+  overlay.classList.remove('open');
+}
+
+buttons.forEach(button => button.addEventListener('click', open));
+overlay.addEventListener('click', close);
+
---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+	$(document).scroll(function () {
+	  var $nav = $(".navbar");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+  });
+
+ document.addEventListener("click",function (e){
+   if(e.target.classList.contains("gallery-item")){
+   	  const src = e.target.getAttribute("src");
+   	  document.querySelector(".modal-img").src = src;
+   	  const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+   	  myModal.show();
+   }
+ })
+
