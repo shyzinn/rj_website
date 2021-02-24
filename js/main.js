7607e1da540724df7866282065472d24dfa66f61
shyzinn <gdesouza0688@gmail.com>
2021-02-24 18:38:10 +0000

    New_changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+// SCROLL FUNCTION 
 $(function () {
 	$(document).scroll(function () {
 	  var $nav = $(".navbar");
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
   });
+
+
+// SCRIPT FOR A EVENT LISTENER ON THE GALLERY 
 
  document.addEventListener("click",function (e){
    if(e.target.classList.contains("gallery-item")){
